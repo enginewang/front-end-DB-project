5c53e426428ec88fc0878dea937aa2c64c56f14a
PtNan <imasxu@gmail.com>
2019-05-23 22:34:58 +0800

    add router

---
+++ db-front/src/views/warehouse/Warehouse.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>warehouse, fuck you world</div>
+</template>
+
+<script>
+export default {
+  name: 'WareHouse'
+}
+</script>
+
+<style lang="less" scoped>
+@import '~ant-design-vue/lib/style/themes/default.less';
+</style>
