64fe3694028706381d27ccc39cd54db853cb70b7
PtNan <819305740@qq.com>
2019-07-07 00:24:25 +0800

    lint

--- db-front/src/views/warehouse/Map.vue
+++ db-front/src/views/warehouse/Map.vue
@@ -13,7 +13,7 @@
           @change="onChange"
           treeCheckable
           :showCheckedStrategy="SHOW_PARENT"
-          searchPlaceholder='Please select'
+          searchPlaceholder="Please select"
           style="width:100%;"
         />
       </a-layout-sider>
@@ -36,8 +36,8 @@ const treeData = [{
   children: [{
     title: 'Child Node1',
     value: '0-0-0',
-    key: '0-0-0',
-  }],
+    key: '0-0-0'
+  }]
 }, {
   title: 'Node2',
   value: '0-1',
@@ -46,54 +46,54 @@ const treeData = [{
     title: 'Child Node3',
     value: '0-1-0',
     key: '0-1-0',
-    disabled: true,
+    disabled: true
   }, {
     title: 'Child Node4',
     value: '0-1-1',
-    key: '0-1-1',
+    key: '0-1-1'
   }, {
     title: 'Child Node5',
     value: '0-1-2',
-    key: '0-1-2',
-  }],
+    key: '0-1-2'
+  }]
 }]
 
-function refreshMapContainer(){
-  var map_container = document.getElementById("pleaseresize")
-  var h = document.getElementsByClassName("ant-layout-content")[0].offsetHeight
-  map_container.setAttribute('style', 'height:' + h + 'px;')
+function refreshMapContainer () {
+  var mapContainer = document.getElementById('pleaseresize')
+  var h = document.getElementsByClassName('ant-layout-content')[0].offsetHeight
+  mapContainer.setAttribute('style', 'height:' + h + 'px;')
 }
 
 const startPoint = new BMap.Point(116.404, 39.915)
 
-function initialize() {  
-  const map = new BMap.Map("map")
+function initialize () {
+  const map = new BMap.Map('map')
   window.map = map
   window.map.centerAndZoom(startPoint, 15)
-  map.enableScrollWheelZoom(true);
-	// 覆盖区域图层测试
-	map.addTileLayer(new BMap.PanoramaCoverageLayer());
+  map.enableScrollWheelZoom(true)
+  // 覆盖区域图层测试
+  map.addTileLayer(new BMap.PanoramaCoverageLayer())
 
-	var stCtrl = new BMap.PanoramaControl(); //构造全景控件
-	stCtrl.setOffset(new BMap.Size(20, 20));
-	map.addControl(stCtrl);//添加全景控件
+  var stCtrl = new BMap.PanoramaControl() // 构造全景控件
+  stCtrl.setOffset(new BMap.Size(20, 20))
+  map.addControl(stCtrl)// 添加全景控件
   var loadCount = 1
-  map.addEventListener("tilesloaded",function(){
-    if(loadCount == 1){
-        map.setCenter(startPoint)
-    }       
+  map.addEventListener('tilesloaded', function () {
+    if (loadCount === 1) {
+      map.setCenter(startPoint)
+    }
     loadCount = loadCount + 1
   })
 }
 
-function addTestWarehouseMarker(){
+function addTestWarehouseMarker () {
   var pt = new BMap.Point(116.417, 39.909)
-  var myIcon = new BMap.Icon(warehouseMarker, new BMap.Size(70,70))
-  var marker = new BMap.Marker(startPoint, {icon: myIcon})
+  var myIcon = new BMap.Icon(warehouseMarker, new BMap.Size(70, 70))
+  var marker = new BMap.Marker(startPoint, { icon: myIcon })
   window.map.addOverlay(marker)
-	var marker2 = new BMap.Marker(pt,{icon:myIcon})  // 创建标注
-  window.map.addOverlay(marker2)           // 将标注添加到地图中
-  console.log("Test Marker added!")
+  var marker2 = new BMap.Marker(pt, { icon: myIcon })// 创建标注
+  window.map.addOverlay(marker2)// 将标注添加到地图中
+  console.log('Test Marker added!')
 }
 
 window.initialize = initialize
@@ -104,22 +104,22 @@ export default {
     return {
       value: ['0-0-0'],
       treeData,
-      SHOW_PARENT,
+      SHOW_PARENT
     }
   },
   methods: {
-    loadScript() {  
-      var script = document.createElement("script")
-      script.src = "http://api.map.baidu.com/api?v=2.0&ak=CvqBqDrjbtb6R31TBqy5zCFz1fUtuNZQ&callback=initialize"
+    loadScript () {
+      var script = document.createElement('script')
+      script.src = 'http://api.map.baidu.com/api?v=2.0&ak=CvqBqDrjbtb6R31TBqy5zCFz1fUtuNZQ&callback=initialize'
       document.body.appendChild(script)
-      console.log("script appended")  
+      console.log('script appended')
     },
     onChange (value) {
       console.log('onChange ', value)
       this.value = value
     }
   },
-  mounted(){
+  mounted () {
     // this.loadScript()
     refreshMapContainer()
     window.onresize = () => {
@@ -159,7 +159,4 @@ export default {
     width: 100%;
     height: 100%;
   } */
-
-
 </style>
-
