7b1f4bfbfe21b83b07e3d6fdf41d422764ae91ab
enginewang <engine74396@gmail.com>
2019-07-07 11:41:34 +0800

    update warehouse mock

--- db-front/src/mock/services/warehouse.js
+++ db-front/src/mock/services/warehouse.js
@@ -30,8 +30,81 @@ const warehouseDetail = () => {
   return [
     {
       'key': 1,
-      'id': 1,
+      'id': 10001,
+      'warehouse_id': 1,
       'model': 'GTX1080',
+      'type': 'Equipment',
+      'number': 1000
+    },
+    {
+      'key': 2,
+      'id': 10002,
+      'warehouse_id': 1,
+      'model': 'GTX2080',
+      'type': 'Equipment',
+      'number': 2000
+    },
+    {
+      'key': 3,
+      'id': 10003,
+      'warehouse_id': 2,
+      'model': 'GTX3080',
+      'type': 'Equipment',
+      'number': 1000
+    },
+    {
+      'key': 4,
+      'id': 10004,
+      'warehouse_id': 2,
+      'model': 'GTX1080',
+      'type': 'Equipment',
+      'number': 1000
+    },
+    {
+      'key': 5,
+      'id': 10005,
+      'warehouse_id': 2,
+      'model': 'GTX980',
+      'type': 'Equipment',
+      'number': 1000
+    },
+    {
+      'key': 6,
+      'id': 10006,
+      'warehouse_id': 3,
+      'model': 'GTX3080',
+      'type': 'Equipment',
+      'number': 1000
+    },
+    {
+      'key': 7,
+      'id': 10007,
+      'warehouse_id': 3,
+      'model': 'GTX2080',
+      'type': 'Equipment',
+      'number': 1000
+    },
+    {
+      'key': 8,
+      'id': 10008,
+      'warehouse_id': 3,
+      'model': 'GTX1080',
+      'type': 'Equipment',
+      'number': 1000
+    },
+    {
+      'key': 9,
+      'id': 10009,
+      'warehouse_id': 3,
+      'model': 'i7-8700k',
+      'type': 'Equipment',
+      'number': 1000
+    },
+    {
+      'key': 10,
+      'id': 10010,
+      'warehouse_id': 3,
+      'model': 'i7-8700k',
       'type': 'Equipment',
       'number': 1000
     }
