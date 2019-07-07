904b88936fe9950a078bb350a84241a443906869
PtNan <819305740@qq.com>
2019-07-07 10:02:07 +0800

    disable eslint

--- db-front/vue.config.js
+++ db-front/vue.config.js
@@ -97,7 +97,7 @@ module.exports = {
 
   // disable source map in production
   productionSourceMap: false,
-  lintOnSave: undefined,
+  lintOnSave: false,
   // babel-loader no-ignore node_modules/*
   transpileDependencies: []
 }
