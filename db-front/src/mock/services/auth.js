d04659bab3aaff5f579cb31f05d06e6179aa5261
PtNan <819305740@qq.com>
2019-07-09 17:15:34 +0800

    add LoginAPI

--- db-front/src/mock/services/auth.js
+++ db-front/src/mock/services/auth.js
@@ -12,20 +12,12 @@ const login = ( options ) => {
   }
 
   return builder( {
-    'id': Mock.mock( '@guid' ),
-    'name': Mock.mock( '@name' ),
     'username': 'admin',
     'password': '',
     'avatar': 'https://gw.alipayobjects.com/zos/rmsportal/jZUIxmJycoymBprLOUbT.png',
     'status': 1,
-    'telephone': '',
-    'lastLoginIp': '27.154.74.117',
-    'lastLoginTime': 1534837621348,
-    'creatorId': 'admin',
-    'createTime': 1497160610259,
     'deleted': 0,
     'roleId': 'admin',
-    'lang': 'zh-CN',
     'token': '4291d7da9005377ec9aec4a71ea837f'
   }, '', 200, {'Custom-Header': Mock.mock( '@guid' )} )
 }
