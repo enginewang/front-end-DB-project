d49b6f2d5718f5e210677b7adbe9fa7eb72d6f7a
KHAKhazeus <khazeus@outlook.com>
2019-07-11 14:38:56 +0800

    Map Changed

--- db-front/src/views/warehouse/Map.vue
+++ db-front/src/views/warehouse/Map.vue
@@ -65,7 +65,7 @@ function initialize() {
   map.enableScrollWheelZoom(true)
   // 覆盖区域图层测试
   map.addTileLayer(new BMap.PanoramaCoverageLayer())
-
+  
   var stCtrl = new BMap.PanoramaControl() // 构造全景控件
   stCtrl.setOffset(new BMap.Size(20, 20))
   map.addControl(stCtrl) // 添加全景控件
